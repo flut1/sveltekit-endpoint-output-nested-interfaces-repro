6077a6248703a102a257aa5f75a3914d14103af2
Floris Bernard <floris.bernard@gmail.com>
2022-05-14 16:19:52 +0200

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,33 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+interface GetOutput {
+    item: { message: string }
+}
+
+// No TS error
+export const post: RequestHandler<any, GetOutput> = async () => {
+    return {
+        status: 200,
+        body: {
+            item: { message: 'hello' },
+        },
+    };
+};
+
+interface Item {
+    message: string;
+}
+
+interface PostOutput {
+    item: Item;
+}
+
+// TS error: Type '{ message: string; }' is not assignable to type 'never'.ts(2322)
+export const get: RequestHandler<any, PostOutput> = async () => {
+    return {
+        status: 200,
+        body: {
+            item: { message: 'hello' },
+        },
+    };
+};
